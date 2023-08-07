f5f93f034b3a726593d84bf859c9a9c69c7be6a6
CodeCrusader <130312133+CodeCrusader80@users.noreply.github.com>
2023-08-07 17:53:06 +0200

    Allow to recover an image
    Implementation of data recovery in the application, with an image in the background

--- src/API/tv-shows.js
+++ src/API/tv-shows.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=d48c63199d6b5c860eda4ddf7c97c701";
+import {BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL} from "../config.js";
+
 export class TVShowAPI {
     static async fetchPopulars() {
     const response = await axios.get(`${BASE_URL}movie/popular${API_KEY_PARAM}`);
-    console.log("***", response.data.results);
     return response.data.results;
     }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import "./global.css"
 import s from "./style.module.css"
 import {TVShowAPI} from "./API/tv-shows.js";
+import {useEffect, useState} from "react";
+import {BACKDROP_BASE_URL} from "./config.js";
 
 TVShowAPI.fetchPopulars()
 export function App() {
+    const [currentMovie, setCurrentMovie] = useState();
+
+    async function fetchPopulars() {
+        const populars = await TVShowAPI.fetchPopulars();
+        if (populars.length > 0) {
+            setCurrentMovie(populars[0]);
+        }
+    }
+    useEffect(() => {
+        fetchPopulars();
+    }, []);
+
+    console.log("***", currentMovie);
+
   return (
-      <div className={s.main_container}>
+      <div className={s.main_container} style={{background: currentMovie ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentMovie.backdrop_path}") no-repeat center/ cover` : "#0A0C10"}}>
           <div className={s.header}>
               <div className="row">
                   <div className="col-4">
@@ -17,8 +33,8 @@ export function App() {
                   </div>
               </div>
           </div>
-          <div className={s.tv_show_detail}>Detail</div>
-          <div className={s.recommendations}>Recommendations</div>
+          <div className={s.tv_show_detail}>Details</div>
+          <div className={s.recommendations}>Recommended Movies</div>
       </div>
   );
 }
